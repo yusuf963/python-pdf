c9bf0c01994a7e483aa4a55cc9ffb2d47d700313
yusuf963 <mohammad963yusuf@gmail.com>
2022-10-13 12:42:40 +0100

    uplaod new file

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
 from PyPDF2 import PdfReader
 import pyttsx3
 
-reader = PdfReader("mat67-Common_Math_Symbols.pdf")
+reader = PdfReader("bbc_contract.pdf")
 number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
-print(text)
-# engine = pyttsx3.init()
-# engine.say(text)
-# engine.runAndWait()
+for i in range(number_of_pages):
+    page = reader.pages[i]
+    text = page.extract_text()
+    # print(text)
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
